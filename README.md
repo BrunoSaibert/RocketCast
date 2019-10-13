55477e389985f0834da8bc91878aced1ee7ec230
BrunoSaibert <brunosaibert@gmail.com>
2019-10-12 21:36:07 -0300

    feature: Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Trabalhando com HTML Audio e JavaScript
+
+## RocketCast
+
+### Easy
+
+- [x] Iniciar a estrutura do layout (HTML e CSS).
+- [x] Criar o player de audio.
+- [x] Tocar automaticamente quando a janela abrir.
+- [x] Remover o auto play do player criado.
+- [x] Alimentar o player com os dados de audio.
+
+### Moderate
+
+- [ ] Refatorar: Criar um objeto para guardar o estado, funcionalidades e configurações do player.
+- [ ] Quando o audio finalizar, iniciar o próximo da lista.
+- [ ] Quando chegar no último audio, voltar a reproduzir o primeiro.
+
+### Hard
+
+- [ ] .
+- [ ] .
+- [ ] .
+- [ ] .
+- [ ] .
+- [ ] .
+- [ ] .
---
+++ index.js
@@ -0,0 +1,18 @@
+const cover = document.querySelector(".card-image");
+const title = document.querySelector(".card-content h5");
+const artist = document.querySelector(".artist");
+const audio = document.querySelector("audio");
+
+const data = {
+  title:
+    "Como começei a programar / Por que criamos a Rocketseat / Nossa Stack",
+  artist: "Diego Fernandes",
+  cover: "como-comecei.jpg",
+  file: "como-comecei.mp3"
+};
+
+cover.style.background = `url('files/${data.cover}') no-repeat center
+center / cover`;
+title.innerText = data.title;
+artist.innerHTML = `<i class="material-icons">account_circle</i> ${data.artist}`;
+audio.src = data.file;
